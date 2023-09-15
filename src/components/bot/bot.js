0f0ad9bfdc998e9faf00baeff2813facb066cef9
Nareshimv <nareshhr@imversion.com>
2023-09-15 15:45:42 +0530

    new react update

---
+++ src/components/bot/bot.js
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+		<div style={{backgroundColor:"red",height:'100px',width:'100px'}}>
+			Bot
+		</div>
+	)
+}
+export default Header;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/components/bot/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bot.js',
+    library: 'talkbase-embedded-chat-bot',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
